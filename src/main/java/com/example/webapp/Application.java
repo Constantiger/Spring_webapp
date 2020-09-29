2f920fdaea078c7a7dd60336f437ff4f65921f80
Constantiger <constantiger@users.noreply.github.com>
2020-09-29 20:56:45 +0300

    swagger dependencies

--- src/main/java/com/example/webapp/Application.java
+++ src/main/java/com/example/webapp/Application.java
@@ -2,10 +2,12 @@ package com.example.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
