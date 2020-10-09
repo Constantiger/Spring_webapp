c28a86d8b69f2af350e9ec508dd4e3655285cea1
Constantiger <constantiger@users.noreply.github.com>
2020-10-09 12:40:05 +0300

    check imports

--- src/main/java/com/example/webapp/repos/ProductRepo.java
+++ src/main/java/com/example/webapp/repos/ProductRepo.java
@@ -3,8 +3,6 @@ package com.example.webapp.repos;
 import com.example.webapp.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface ProductRepo extends CrudRepository<Product, Long> {
     Iterable<Product> findByProductType(String productType);
 }
